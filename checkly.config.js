bf1cc11fc8f74b8478dc4de4c05ed1b4d8f514f5
Sam Bordo <sambordo1@gmail.com>
2023-05-15 15:25:11 -0400

    add checkly checks

---
+++ checkly.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('checkly')
+
+module.exports = defineConfig({
+    projectName: 'Personal Website Monitoring',
+    logicalId: 'website-monitoring-1',
+    repoUrl: 'https://github.com/sambordo1/checkly',
+    checks: {
+      activated: true,
+      muted: false,
+      runtimeId: '2022.10',
+      frequency: 120,
+      locations: ['us-east-1', 'us-east-2', 'us-west-1','us-west-2'],
+      tags: ['website', 'api'],
+      checkMatch: '**/__checks__/**/*.check.js',
+      ignoreDirectoriesMatch: [],
+      browserChecks: {
+        frequency: 10,
+        testMatch: '**/__checks__/**/*.spec.js',
+      },
+    },
+    cli: {
+      runLocation: 'us-east-1',
+    }
+   });
--- tests/color-change.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('change colors on website', async ({ page }) => {
-  await page.goto('/');
-  await page.locator('#blue-mode').click();
-  await page.locator('#green-mode').click();
-  await page.locator('#purple-mode').click();
-});
